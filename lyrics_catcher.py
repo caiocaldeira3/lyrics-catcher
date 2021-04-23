ba5d56d0485ab4eac8470d90932e2bb2e2e72700
caiocaldeira3 <caio.caldeira3@gmail.com>
2021-04-22 22:29:00 -0300

    Add initial python script with editable bash script

---
+++ lyrics_catcher.py
@@ -0,0 +1,57 @@
+import dotenv
+import json
+import os
+import requests
+import sys
+
+import dataclasses as dc
+import tkinter as tk
+
+from pathlib import Path
+
+base_path = Path(__file__).resolve().parent
+dotenv.load_dotenv(base_path / ".env", override=False)
+
+@dc.dataclass(init=True)
+class LyricsWindow:
+
+    root: tk.Tk = dc.field(init=False, default=None)
+
+    lyrics: str = dc.field(init=True)
+    title: str = dc.field(init=True)
+
+    def __post_init__ (self) -> None:
+        self.root = tk.Tk()
+
+        self.root.title(self.title)
+        self.root.resizable(width=True, height=True)
+
+        T = tk.Text(self.root, bg="#333")
+
+        T.pack(expand=True, fill=tk.BOTH)
+        T.tag_config("lyrics", foreground="#999")
+        T.insert(tk.END, self.lyrics, "lyrics")
+
+        T.config(state=tk.DISABLED)
+
+        self.root.mainloop()
+
+def load_song():
+
+    artist = sys.argv[1]
+    music = sys.argv[2]
+
+    key = os.environ["APPKEY"]
+
+    url = f"https://api.vagalume.com.br/search.php?art={artist}&mus={music}&apikey={key}"
+    resp = requests.post(url)
+
+    resp_j = json.loads(resp.text)
+
+    return resp_j["mus"][0]["text"], artist + " " + music
+
+if __name__ == "__main__":
+
+    lyrics, title = load_song()
+
+    window = LyricsWindow(lyrics, title)
